352d25a26e7cfdf1884dc0e86cfd85a809b9a0d4
David Kemp <19152940+davaya@users.noreply.github.com>
2022-04-02 14:12:41 -0400

    update intro

--- pages/index.js
+++ pages/index.js
@@ -21,14 +21,12 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>PACE deployment test</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        <p>PACE deployment test using the
+          <a href="https://nextjs.org/learn"> Next.js</a> UI framework.
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
+        {/* <h2 className={utilStyles.headingLg}>Blog</h2> */}
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
